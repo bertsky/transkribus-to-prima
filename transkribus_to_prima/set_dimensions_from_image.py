043b04ee0b6d595eea8c2e5ab2ad405d1c9dff18
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2025-12-04 20:48:54 +0100

    page-dimensions-from-page: use Pillow instead of ImageMagick CLI

--- transkribus_to_prima/set_dimensions_from_image.py
+++ transkribus_to_prima/set_dimensions_from_image.py
@@ -1,16 +1,17 @@
 from lxml import etree as ET
 from click import command, argument
-from subprocess import check_output
+from PIL import Image
+
 
 @command()
 @argument('inpage')
 @argument('image')
 @argument('outpage')
 def cli(inpage, image, outpage):
-    width, height = check_output(['identify', '-format',  '%w %h', image], encoding='utf-8').split(' ')
+    width, height = Image.open(image).size
     tree = ET.parse(inpage)
     el_page = tree.xpath('*[local-name()="Page"]')[0]
-    el_page.set('imageWidth', width)
-    el_page.set('imageHeight', height)
+    el_page.set('imageWidth', str(width))
+    el_page.set('imageHeight', str(height))
     tree.write(outpage, encoding='utf-8')
 
