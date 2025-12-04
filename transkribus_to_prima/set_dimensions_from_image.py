f2631c83d2516948006117de57bbb3b6d20bde17
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2025-12-04 21:12:34 +0100

    page-dimensions-from-image: optionally update @imageFilename

--- transkribus_to_prima/set_dimensions_from_image.py
+++ transkribus_to_prima/set_dimensions_from_image.py
@@ -1,13 +1,14 @@
 from lxml import etree as ET
-from click import command, argument, Path
+from click import command, argument, option, Path
 from PIL import Image
 
 
 @command(context_settings={'help_option_names': ['-h', '--help']})
+@option('-F', '--replace-filename', help="also update the @imageFilename", is_flag=True)
 @argument('inpage', type=Path(exists=True, dir_okay=False))
 @argument('image', type=Path(exists=True, dir_okay=False))
 @argument('outpage', type=Path(exists=False, dir_okay=False))
-def cli(inpage, image, outpage):
+def cli(replace_filename, inpage, image, outpage):
     """
     Reads a PAGE-XML file INPAGE, and respective image file IMAGE,
     updates the Page/@imageWidth and @imageHeight from the image,
@@ -18,5 +19,7 @@ def cli(inpage, image, outpage):
     el_page = tree.xpath('*[local-name()="Page"]')[0]
     el_page.set('imageWidth', str(width))
     el_page.set('imageHeight', str(height))
+    if replace_filename:
+        el_page.set('imageFilename', image)
     tree.write(outpage, encoding='utf-8')
 
